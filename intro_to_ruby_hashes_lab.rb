dbb1e4149191811a5e5166c29820ff4dae0c6ed3
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-19 04:46:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,25 +1,31 @@
 def new_hash
-  # return an empty hash
+  new_hash = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  my_has = {name: "Kai", flatiron: "DC"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {:name => "Grace Hopper"}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  id_generator = {:id => 15702}
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  my_hash_creator = {key => value}
+  my_hash_creator
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  read_from_hash = hash[key]
 end
 
 def update_counting_hash(hash, key)
